253ec47311adc424567d9ffdb948c458ceed63fa
Hafidzil Khairi <hafidzilkhairi00@gmail.com>
2020-02-18 10:24:14 +0700

    Changed content to fawaid website's content

---
+++ precache-manifest.b1be3d56fc9f62dc783c79e2350233ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5dda51222c7c02e7600ffbd9c4005e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b097106c455291fd53a1",
+    "url": "/static/js/2.9edf062a.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.9edf062a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c054564b31100d30b0fb",
+    "url": "/static/js/main.1d210add.chunk.js"
+  },
+  {
+    "revision": "325f2eb088466e9e7d87",
+    "url": "/static/js/runtime-main.2d510ab3.js"
+  },
+  {
+    "revision": "f24cbb33c1910fae5d4a5e028b9c52c0",
+    "url": "/static/media/artikel.f24cbb33.jpg"
+  },
+  {
+    "revision": "d4f6a0aae452b65df9421324e24da973",
+    "url": "/static/media/logo-fawaid-ilmu-white.d4f6a0aa.png"
+  },
+  {
+    "revision": "9ff79d128f3451e33157a685081d0dc8",
+    "url": "/static/media/search.9ff79d12.svg"
+  }
+]);
